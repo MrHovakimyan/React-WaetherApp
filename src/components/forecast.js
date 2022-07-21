9c2737f2b0a848da91940990f328697161715205
Hayk Hovakimyan <hghovakimyan@outlook.com>
2022-07-21 14:34:45 +0400

    feat: forecast panel w/additional info created/styled

--- src/components/forecast.js
+++ src/components/forecast.js
@@ -20,6 +20,7 @@ const Forecast = ({ data }) => {
     <>
       <label className="title">Daily</label>
       <Accordion allowZeroExpanded>
+        {/* display upcoming 7 days forecast  */}
         {data.list.splice(0, 7).map((item, index) => (
           <AccordionItem key={index}>
             <AccordionItemHeading>
@@ -38,7 +39,36 @@ const Forecast = ({ data }) => {
                 </div>
               </AccordionItemButton>
             </AccordionItemHeading>
-            <AccordionItemPanel></AccordionItemPanel>
+
+            {/* opens panel w/additional info */}
+            <AccordionItemPanel>
+              <div className="daily-details-grid">
+                <div className="daily-details-grid-item">
+                  <label>Pressure:</label>
+                  <label>{item.main.pressure} hPa</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Humidity:</label>
+                  <label>{item.main.humidity} %</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Clouds:</label>
+                  <label>{item.clouds.all} %</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Wind speed:</label>
+                  <label>{item.wind.speed} m/s</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Sea Level:</label>
+                  <label>{item.main.sea_level} m</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Feels Like:</label>
+                  <label>{Math.round(item.main.feels_like)} °C</label>
+                </div>
+              </div>
+            </AccordionItemPanel>
           </AccordionItem>
         ))}
       </Accordion>
